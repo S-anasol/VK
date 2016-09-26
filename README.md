b95f37d7c06929adb4c5c2532bbe14f657920c70
Sanasol <sanasol2008@yandex.ru>
2016-09-27 00:20:57 +0300

    add newfeatures

--- README.md
+++ README.md
@@ -1,70 +1,14 @@
-# VK
+# VK API Wrapper
 
-## По-русски
+The PHP class for vk.com API and OAuth.
 
-Класс реализует VK API и авторизацию по OAuth протоколу.
-По всем вопросам можно писать на <vladkens@yandex.ru>
-
-### Использование
-1. Подключите класс
-
-        require('VK.php');
-
-2. Создайте объект VK
-    1. без авторизации
-
-            $vk = new VK\VK('{APP_ID}', '{API_SECRET}');
-
-    2. с авторизацией
-
-            $vk = new VK\VK('{APP_ID}', '{API_SECRET}', '{ACCESS_TOKEN}');
-
-3. Если нужна авторизация
-    1. Получаем ссылку авторизации
-
-            $vk->getAuthorizeURL('{API_SETTINGS}', '{CALLBACK_URL}');
-
-    2. Получаем токен доступа по ключу из ссылки авторизации
-
-            $vk->getAccessToken('{CODE}');
-            
-    3. Проверить авторизирован ли пользователь
-            
-            $vk->isAuth(); // return bool
-
-4. Используем API
-
-        $vk->api('{METHOD_NAME}', '{PARAMETERS}');
-        
-### Другие методы
-* Установить версию API.
-    `$vk->setApiVersion({NUBMER});`
-    
-### Переменные
-* `{APP_ID}` — ID приложения вконтакте.
-* `{API_SECRET}` — Секретный код приложения.
-* `{ACCESS_TOKEN}` — Токен доступа.
-* `{API_SETTINGS}` — Запрашиваемые [права доступа](http://vk.com/developers.php?oid=-1&p=Права_доступа_приложений) приложения (через запятую).
-* `{CALLBACK_URL}` — Адрес, на который будет передан `{CODE}`.
-* `{CODE}` — Код для получения токена доступа.
-* `{METHOD_NAME}` — Имя API метода. [Все методы](http://vk.com/developers.php?oid=-1&p=Описание_методов_API).
-* `{PARAMETERS}` — Параметры соответствующего метода API.
-
-\* Если нужно получить бесконечный токен, используете параметр `offline` в `{API_SETTINGS}`.
-
-### Лицензия
-[MIT](https://raw.github.com/vladkens/VK/master/LICENSE)
-
-## English
-
-The PHP class for vk.com API and to support OAuth.
-You can ask me any questions by e-mail: <vladkens@yandex.ru>
+Features: HTTP proxy, captcha recognition via antigate, api response timeout limit
 
 ### Use
 1. Connect class
 
         require('VK.php');
-        
+
 2. Create VK object
     1. without authorization
 
@@ -82,19 +26,29 @@ You can ask me any questions by e-mail: <vladkens@yandex.ru>
     2. Get the token access by code from the authoriz link
 
             $vk->getAccessToken('{CODE}');
-            
+
     3. Check the status of authorization
-            
+
             $vk->isAuth(); // return bool
-            
+
 4. Usage API
 
         $vk->api('{METHOD_NAME}', '{PARAMETERS}');
 
-### Other methods
-* Set version of API.
-    `$vk->setApiVersion({NUBMER});`
-    
+5. Captcha recognition
+
+  ```
+  $vk->setAntigate('antigate-key');
+  $vk->setCaptchaSavePath('./captcha'); // YOU MUST create 'captcha' folder at project root path
+  ```
+
+### Class methods
+* `$vk->setApiVersion({NUBMER});` - set api version
+* `$vk->setProxy("1.2.3.4:8080");` - set HTTP proxy for API requests
+* `$vk->setTimeout(10);` - set API Response timeout
+* `$vk->setAntigate("antigate-key");` - set [antigate](http://antigate.com/) API key to resolve captcha
+* `$vk->setCaptchaSavePath("/tmp");` - set temporary captcha image save path
+
 ### Variables
 * `{APP_ID}` — Your application's identifier.
 * `{API_SECRET}` — Secret application key.
--- src/VK/VK.php
+++ src/VK/VK.php
@@ -41,6 +41,31 @@ class VK
     private $auth = false;
 
     /**
+     * HTTP Proxy.
+     * Format: 127.0.0.1:8080
+     * @var string
+     */
+    private $proxy = false;
+
+    /**
+     * API Response timeout in seconds
+     * @var integer
+     */
+    private $timeout = 5;
+
+    /**
+     * Antigate API key
+     * @var integer
+     */
+    private $antigate_key = false;
+
+    /**
+     * Temporary captcha image save path
+     * @var string
+     */
+    private $captcha_path = '/tmp';
+
+    /**
      * Instance curl.
      * @var Resource
      */
@@ -84,6 +109,46 @@ class VK
     }
 
     /**
+     * Set HTTP proxy.
+     * @param   string $proxy
+     * @return  void
+     */
+    public function setProxy($proxy)
+    {
+        $this->proxy = $proxy;
+    }
+
+    /**
+     * Set API Response timeout.
+     * @param   int $seconds
+     * @return  void
+     */
+    public function setTimeout($seconds)
+    {
+        $this->timeout = $seconds;
+    }
+
+    /**
+     * Set Antigate API key
+     * @param   int $seconds
+     * @return  void
+     */
+    public function setAntigate($api_Key)
+    {
+        $this->antigate_key = $api_Key;
+    }
+
+    /**
+     * SetCaptcha image save path
+     * @param   int $seconds
+     * @return  void
+     */
+    public function setCaptchaSavePath($path)
+    {
+        $this->captcha_path = $path;
+    }
+
+    /**
      * Set Access Token.
      * @param   string $access_token
      * @throws  VKException
@@ -122,8 +187,9 @@ class VK
             'response_type' => 'code'
         );
 
-        if ($test_mode)
+        if ($test_mode) {
             $parameters['test_mode'] = 1;
+        }
 
         return $this->createUrl(self::AUTHORIZE_URL, $parameters);
     }
@@ -178,7 +244,9 @@ class VK
     public function checkAccessToken($access_token = null)
     {
         $token = is_null($access_token) ? $this->access_token : $access_token;
-        if (is_null($token)) return false;
+        if (is_null($token)) {
+            return false;
+        }
 
         $rs = $this->api('getUserSettings', array('access_token' => $token));
         return isset($rs['response']);
@@ -192,8 +260,12 @@ class VK
      * @param   string $requestMethod
      * @return  mixed
      */
-    public function api($method, $parameters = array(), $format = 'array', $requestMethod = 'get')
+    public function api($method, $parameters = array(), $format = 'array', $requestMethod = 'get', $try = 0)
     {
+        if ($try >= 5) {
+            throw new VKException("Number of attempts reached the limit");
+        }
+
         $parameters['timestamp'] = time();
         $parameters['api_id'] = $this->app_id;
         $parameters['random'] = rand(0, 10000);
@@ -223,7 +295,56 @@ class VK
             $rs = $this->request($this->createUrl(
                 $this->getApiUrl($method, $format == 'array' ? 'json' : $format), $parameters));
         }
-        return $format == 'array' ? json_decode($rs, true) : $rs;
+
+        $response = json_decode($rs, true);
+
+        if (!empty($response["error"])) {
+            if ($response["error"]["error_code"] == 14) {
+                if (!empty($this->antigate_key)) {
+                    $tmpfname = tempnam($this->captcha_path, 'cap');
+                    file_put_contents($tmpfname, file_get_contents($response["error"]["captcha_img"]));
+                    $text = $this->recognize($tmpfname, $this->antigate_key, false, "antigate.com");
+                    unlink($tmpfname);
+                    if (!empty($text)) {
+                        unset($parameters['sig']);
+                        $parameters['captcha_sid'] = $response["error"]["captcha_sid"];
+                        $parameters['captcha_key'] = $text;
+
+                        return $this->api($method, $parameters, $format, $try++);
+                    } else {
+                        throw new VKException("Captcha recognition error");
+                    }
+                } else {
+                    throw new VKException("Captcha recognition required");
+                }
+            }
+
+            /**
+             * Too Many requests error, waiting half second and trying again
+             */
+            if ($response["error"]["error_code"] == 6) {
+                usleep(500000);
+                unset($parameters['sig']);
+                return $this->api($method, $parameters, $format, $try++);
+            }
+
+            /**
+             * Access Token expired or invalid
+             */
+            if ($response["error"]["error_code"] == 5) {
+                throw new VKException("User authorization failed. Access Token expired or invalid.");
+            }
+        }
+
+        if (curl_getinfo($this->ch, CURLINFO_HTTP_CODE) != 200) {
+            throw new VKException("Response error HTTP Code: ".curl_getinfo($this->ch, CURLINFO_HTTP_CODE));
+        }
+
+        if ($rs == false) {
+            throw new VKException('Curl error: '.curl_error($this->ch));
+        } else {
+            return $format == 'array' ? json_decode($rs, true) : $rs;
+        }
     }
 
     /**
@@ -247,19 +368,144 @@ class VK
      */
     private function request($url, $method = 'GET', $postfields = array())
     {
-        curl_setopt_array($this->ch, array(
-            CURLOPT_USERAGENT => 'VK/1.0 (+https://github.com/vladkens/VK))',
+        $options = [
+            CURLOPT_USERAGENT => 'VK/1.1 (+https://github.com/S-anasol/VK)',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_SSL_VERIFYPEER => false,
             CURLOPT_POST => ($method == 'POST'),
             CURLOPT_POSTFIELDS => $postfields,
-            CURLOPT_URL => $url
-        ));
+            CURLOPT_URL => $url,
+            CURLOPT_TIMEOUT => $this->timeout
+        ];
+
+        if (!empty($this->proxy)) {
+            $options[CURLOPT_PROXY] = $this->proxy;
+        }
+
+        curl_setopt_array($this->ch, $options);
 
         return curl_exec($this->ch);
     }
 
+    /*
+      $filename - file path to captcha. MUST be local file. URLs not working
+      $apikey   - account's API key
+      $rtimeout - delay between captcha status checks
+      $mtimeout - captcha recognition timeout
+
+      $is_verbose - false(commenting OFF),  true(commenting ON)
+
+      additional custom parameters for each captcha:
+      $is_phrase - 0 OR 1 - captcha has 2 or more words
+      $is_regsense - 0 OR 1 - captcha is case sensetive
+      $is_numeric -  0 OR 1 - captcha has digits only
+      $min_len    -  0 is no limit, an integer sets minimum text length
+      $max_len    -  0 is no limit, an integer sets maximum text length
+      $is_russian -  0 OR 1 - with flag = 1 captcha will be given to a Russian-speaking worker
+
+      usage examples:
+      $text=recognize("/path/to/file/captcha.jpg","YOUR_KEY_HERE",true, "antigate.com");
+
+      $text=recognize("/path/to/file/captcha.jpg","YOUR_KEY_HERE",false, "antigate.com");
+
+      $text=recognize("/path/to/file/captcha.jpg","YOUR_KEY_HERE",false, "antigate.com",1,0,0,5);
+
+      */
+      public function recognize(
+                  $filename,
+                  $apikey,
+                  $is_verbose = true,
+                  $domain="anti-captcha.com",
+                  $rtimeout = 5,
+                  $mtimeout = 120,
+                  $is_phrase = 0,
+                  $is_regsense = 0,
+                  $is_numeric = 0,
+                  $min_len = 0,
+                  $max_len = 0,
+                  $is_russian = 0
+                  ) {
+          if (!file_exists($filename)) {
+              if ($is_verbose) {
+                  echo "file $filename not found\n";
+              }
+              return false;
+          }
+          $file = new CURLFile($filename);
+          $postdata = array(
+              'method'    => 'post',
+              'key'       => $apikey,
+              'file'      => $file,
+              'phrase'    => $is_phrase,
+              'regsense'    => $is_regsense,
+              'numeric'    => $is_numeric,
+              'min_len'    => $min_len,
+              'max_len'    => $max_len,
+                'is_russian'    => $is_russian
+          );
+          $ch = curl_init();
+          curl_setopt($ch, CURLOPT_URL,             "http://$domain/in.php");
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER,     1);
+          curl_setopt($ch, CURLOPT_TIMEOUT,             60);
+          curl_setopt($ch, CURLOPT_POST,                 1);
+          curl_setopt($ch, CURLOPT_POSTFIELDS,         $postdata);
+          $result = curl_exec($ch);
+          if (curl_errno($ch)) {
+              if ($is_verbose) {
+                  echo "CURL returned error: ".curl_error($ch)."\n";
+              }
+              return false;
+          }
+          curl_close($ch);
+          if (strpos($result, "ERROR")!==false) {
+              if ($is_verbose) {
+                  echo "server returned error: $result\n";
+                  mail('mail@sanasol.ws', 'antigate', "server returned error: $result\n");
+              }
+              return false;
+          } else {
+              $ex = explode("|", $result);
+              $captcha_id = $ex[1];
+              if ($is_verbose) {
+                  echo "captcha sent, got captcha ID $captcha_id\n";
+              }
+              $waittime = 0;
+              if ($is_verbose) {
+                  echo "waiting for $rtimeout seconds\n";
+              }
+              sleep($rtimeout);
+              while (true) {
+                  $result = file_get_contents("http://$domain/res.php?key=".$apikey.'&action=get&id='.$captcha_id);
+                  if (strpos($result, 'ERROR')!==false) {
+                      if ($is_verbose) {
+                          echo "server returned error: $result\n";
+                      }
+                      return false;
+                  }
+                  if ($result=="CAPCHA_NOT_READY") {
+                      if ($is_verbose) {
+                          echo "captcha is not ready yet\n";
+                      }
+                      $waittime += $rtimeout;
+                      if ($waittime>$mtimeout) {
+                          if ($is_verbose) {
+                              echo "timelimit ($mtimeout) hit\n";
+                          }
+                          break;
+                      }
+                      if ($is_verbose) {
+                          echo "waiting for $rtimeout seconds\n";
+                      }
+                      sleep($rtimeout);
+                  } else {
+                      $ex = explode('|', $result);
+                      if (trim($ex[0])=='OK') {
+                          return trim($ex[1]);
+                      }
+                  }
+              }
+
+              return false;
+          }
+      }
 }
-
-;
-
